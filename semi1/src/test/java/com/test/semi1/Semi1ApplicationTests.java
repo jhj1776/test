fb4f8416bedb5e3176300d91c0808cfbf28ebf5e
DESKTOP-0BS1CPD\user1 <jeonghyeonjun@naver.com>
2025-07-01 11:06:44 +0900

    원래 12개여야함 뭔가문젠지모름

---
+++ semi1/src/test/java/com/test/semi1/Semi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.semi1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
